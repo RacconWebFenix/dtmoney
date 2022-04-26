2c8c7073108749aa56faddb2d658f780a154ed57
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-26 09:44:04 -0300

    Componente: TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R1.100</td>
+            <td>Desenvolvimento</td>
+            <td>17/02/2021</td>
+          </tr>
+      
+        </tbody>
+      </table>
+    </Container>
+  );
+}
