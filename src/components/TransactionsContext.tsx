d6ab90c1048f10a62945115447330747f781c752
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-27 16:17:30 -0300

    Finalizando inserção

--- src/components/TransactionsContext.tsx
+++ src/components/TransactionsContext.tsx
@@ -28,7 +28,7 @@ type TransactionInput = Omit<Transaction, "id" | "createdAt">;
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -44,8 +44,14 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post("/transaction", transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post("/transaction", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
